f91f614319f80ee2049ab9f59f54f7a68f5e2bba
janvi10026 <137758205+janvi10026@users.noreply.github.com>
2025-12-28 15:24:07 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score =  "Janvi"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;  false=> 0
+
+let isLoggedIn = "Janvi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+  
+// 1 => true; 0 => false
+// "" => false
+// "Janvi" => true
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
