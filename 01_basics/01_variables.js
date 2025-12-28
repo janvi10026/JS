47c55327410dc589f504ab13b22e4e178253cedb
janvi10026 <137758205+janvi10026@users.noreply.github.com>
2025-12-28 14:52:38 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 1254
+let accountEmail = "janvi04@gmail.com"
+var accountPassword = "5789"
+acxountCity = "Jhansi"
+
+// accountId = 5 not allowed
+
+accountEmail = "janvipatwa4@gmail.com"
+accountPassword = "1234"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountEmail)
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Janvi")
+
+let name = "Janvi"
+let age = 20
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// underfined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
