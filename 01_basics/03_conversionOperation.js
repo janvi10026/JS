86c0ffe8e780baf1d22710bf43f1834412d4c89d
janvi10026 <137758205+janvi10026@users.noreply.github.com>
2025-12-29 15:02:55 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,36 @@ let someNumber = 22
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ***********************Operations*************************
+
+let value = 2
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = "janvi"
+let str3 = str1 + str2
+console.log(str3); 
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(3 + 3 + "1");
+
+let num1, num2, num3
+
+num1 =  num2 = num3
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
