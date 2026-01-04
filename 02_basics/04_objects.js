3a14fb317a15bdf91f25211decdfd69d5742c4c8
janvi10026 <137758205+janvi10026@users.noreply.github.com>
2026-01-04 18:06:44 +0000

    object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,50 @@
+ // const tinderUser = new object() //singleton object
+ const tinderUser = {} //non singleton object 
+    tinderUser.id = "123abc"
+    tinderUser.name = "Sammy"
+    tinderUser.isLoggedIn = false
+ 
+   //  console.log(tinderUser);
+   
+    const regulaarUser = {
+      email: "someemail@example.com",
+      fullname: {
+         userfullname: {
+            firstname: "Sammy",
+            lastname: "Smith"
+         }
+      }
+    }
+      
+    console.log(regulaarUser.fullname.userfullname.firstname);
+
+
+ const obj1 = {1: "a", 2: "b"}
+ const obj2 = {3: "c", 4: "d"}
+ const obj3 = {5: "e", 6: "f"}
+
+//  const obj4 = Object.assign({}obj1, obj2, obj3)
+const obj4 = {...obj1, ...obj2, ...obj3} //spread operator
+ console.log(obj4);
+
+ const users =[
+   {
+      id: "123",
+      email:"someemail@example.com"
+   },
+   {
+
+   },
+   {
+
+   },     
+ ]
+
+ users[1].email
+ console.log(tinderUser);
+ console.log(Object.keys(tinderUser));
+ console.log(Object.values(tinderUser));
+ console.log(Object.entries(tinderUser));
+ 
+ console.log(tinderUser.hasOwnProperty('isLOggedIn'));
+ 
